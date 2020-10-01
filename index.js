a51de70b687bce19ce8c1f7d7420b24139c00fe2
Jonathan Koss <jonathan_koss@yahoo.com>
2020-10-01 23:30:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ function unleashDog(dogName="Boo Radley", dogBreed="Pibble") {
 let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
 
 function exerciseDog(dogName, dogBreed) {
-  routine.forEach(console.log(routine[i]()));
+  routine.forEach(elementconsole.log(routine[i]()));
 }
