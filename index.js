cea1790552e8962fd6a24c07b8b7053450cfba01
Jonathan Koss <jonathan_koss@yahoo.com>
2020-10-01 23:17:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ function wakeDog(dogName = "Boo Radley", dogBreed) {
   return `Wake ${dogName} the ${dogBreed}`;
 }
 
-function leashDog(dogName, dogBreed) {
+function leashDog(dogName="Boo Radley", dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
   return `Leash ${dogName} the ${dogBreed}`;
 }
 
-function walkToPark(dogName, dogBreed) {
+function walkToPark(dogName="Boo Radley", dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
   return `Walk to the park with ${dogName} the ${dogBreed}`;
 }
 
-function throwFrisbee(dogName, dogBreed) {
+function throwFrisbee(dogName="Boo Radley", dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
   return `Throw the frisbee for ${dogName} the ${dogBreed}`;
 }
 
-function walkHome(dogName, dogBreed) {
+function walkHome(dogName="Boo Radley", dogBreed) {
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
   return `Walk home with ${dogName} the ${dogBreed}`;
 }
 
-function unleashDog(dogName, dogBreed) {
+function unleashDog(dogName="Boo Radley", dogBreed) {
   console.log(`Unleash ${dogName} the ${dogBreed}`);
   return `Unleash ${dogName} the ${dogBreed}`;
 }
